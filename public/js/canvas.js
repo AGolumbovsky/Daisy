37a094668e77a3c624ae95c6f768e44ed9c32a66
AGolumbovsky <agolumbovsky@gmail.com>
2016-01-06 17:05:39 -0500

    started w/ mongoose

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -10,23 +10,12 @@ function drawGrin(moodRate) {
 	// draw the smile
 	ctx.lineWidth = 8;
 	ctx.beginPath();
-	ctx.moveTo(50, 75);
-	ctx.quadraticCurveTo(150, moodRate, 250, 75);
+	// starting point x, y
+	ctx.moveTo(25, 75);
+	// cpX, cpY(dynamic), x(end pt), y(end pt)
+	ctx.quadraticCurveTo(150, moodRate, 275, 75);
 	ctx.stroke();
 
 }
 drawGrin(75);
 
-function drawAvg() {
-	// blah blah
-	drawGrin(120);
-}
-
-function drawTranslate() {
-	// blah blah
-	var userInput = document.getElementById('userInput').value;
-
-	// check for negative numbers, allow zero
-	if(!userInput || userInput>10) alert("How hard is it to enter a value 0-10?");
-	else drawGrin(userInput*15);
-}
