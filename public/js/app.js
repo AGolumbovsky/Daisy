b354ffbe75feb01d9e9dda26dabd491a7e496b06
AGolumbovsky <agolumbovsky@gmail.com>
2016-01-05 20:51:12 -0500

    click on face, see it drawn

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,24 @@
 function happy() {
 	console.log("I'm happy");
-	drawTranslate(8);
+	drawGrin(150);
 }
 
 function mad() {
 	console.log("I'm mad");
+	drawGrin(0);
 }
 
-function alarm() {
-	alert("Achtung!!! Achtung!!!");
+// draw the smiles on the face
+function drawAvg() {
+	// blah blah
+	drawGrin(120);
+}
+
+function drawNow() {
+	// blah blah
+	var userInput = document.getElementById('userInput').value;
+
+	// check for negative numbers, allow zero
+	if(!userInput || userInput>10) alert("How hard is it to enter a value 0-10?");
+	else drawGrin(userInput*15);
 }
