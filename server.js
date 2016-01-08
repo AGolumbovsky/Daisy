51614c06ce817fc874d39228684a1fca3553e21d
AGolumbovsky <agolumbovsky@gmail.com>
2016-01-08 09:11:50 -0500

    app.*

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 // dependencies
 var express = require('express');
+var mongoose = require('mongoose');
 
 // establish the port
 var port = 8080;
 
-// initialize the app
+// connection to db
+mongoose.connect('mongodb://localhost/Daisy');
+var db = mongoose.connection;
+
+// get the app going
 var app = express();
 
 // midolware
 app.use(express.static(__dirname + '/public'));
+
+// endpoints
+app.post('api/moods', function(req, res) {
+	console.log("Server got POST request");
+})
+
 
 // listen carefully
 app.listen(port, function() {
